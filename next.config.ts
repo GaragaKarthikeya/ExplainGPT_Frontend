b8588d6d3c0c098f9b68f036d3af6ab2ceb0d826
Garaga Karthikeya <Garaga.Karthikeya@iiitb.ac.in>
2025-03-31 14:31:30 +0000

    ignore imports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // This allows the build to continue even if ESLint errors exist
+  },
 };
 
 export default nextConfig;
